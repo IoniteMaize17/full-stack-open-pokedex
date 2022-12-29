aef0ec0bb27ab2cf60afb7cd308ab6e588939d4c
Nguyen Duc <mduc0123@gmail.com>
2022-12-29 16:47:17 +0200

    change logic test with cypress

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -5,7 +5,8 @@ describe('Pokedex', function () {
         cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
     })
     it('PokemonPage can be opened', function () {
-        cy.visit('http://localhost:5000/pokemon/venomoth')
+        cy.visit('http://localhost:5000')
+        cy.contains('venomoth').click()
         cy.contains('venomoth')
     })
 })
