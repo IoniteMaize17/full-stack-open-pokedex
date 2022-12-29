e386277902c6084c8abda982c7006340461ce004
Nguyen Duc <mduc0123@gmail.com>
2022-12-29 16:42:44 +0200

    add test with cypress

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,8 @@ describe('Pokedex', function () {
         cy.contains('ivysaur')
         cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
     })
+    it('PokemonPage can be opened', function () {
+        cy.visit('http://localhost:5000/pokemon/venomoth')
+        cy.contains('venomoth')
+    })
 })
